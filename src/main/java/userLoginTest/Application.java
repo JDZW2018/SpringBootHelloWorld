324340ce9a3fe74872d895b0515e08568b2741dd
tfs <949465194@qq.com>
2019-11-25 22:49:05 +0800

    update

--- src/main/java/userLoginTest/Application.java
+++ src/main/java/userLoginTest/Application.java
@@ -1,13 +1,13 @@
-package com.t9vg.SpringBootHelloWorld;
+package userLoginTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootHelloWorldApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
